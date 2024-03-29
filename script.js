ef0804a52376918a2ef17f791eb6efac72b8a0c9
sheng-ri <38691071+sheng-ri@users.noreply.github.com>
2024-03-30 01:18:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MsDocs LangButton
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://learn.microsoft.com/*
