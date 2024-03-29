f1adc07f3868f00b033daf2ed9d88320f1367e14
sheng-ri <38691071+sheng-ri@users.noreply.github.com>
2024-03-30 01:16:35 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         MsDocs LangButton
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://learn.microsoft.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function tryRun(consumer) {
+       let cnt = 0
+       const code = setInterval(() => {
+          if (consumer() || cnt ++ > 4) clearInterval(code)
+        },50)
+    }
+
+    function createButton(a,b,c) {
+        const cnButton = document.createElement("button")
+        cnButton.textContent = c
+        cnButton.onclick = () => {
+            location.href = location.href.replace(a,b)
+        }
+       return cnButton
+    }
+
+    function createChineseButton() {
+
+        tryRun(() => {
+            let menus = document.querySelectorAll(".site-header-brand")
+            if (!menus) return false;
+            for (let menu of menus) {
+                menu.parentNode.append(createButton("us-en","zh-cn","中文"))
+            }
+            return true;
+        })
+    }
+
+    function createEnglishButton() {
+        tryRun(() => {
+            let menus = document.querySelectorAll(".site-header-brand")
+            if (!menus) return false;
+            for (let menu of menus) {
+                menu.parentNode.append(createButton("zh-cn","us-en","英文"))
+            }
+            console.log(1)
+            return true;
+        })
+    }
+    createChineseButton();
+    createEnglishButton();
+    // Your code here...
+})();
